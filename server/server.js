66a98ba11ac3fd44389e37950afd0f780e77638c
Shane Phillips <shane@loongphillips.com>
2022-04-04 17:31:12 -0400

    housekeeping

--- server/server.js
+++ server/server.js
@@ -9,6 +9,14 @@ app.get("/", (req, res) => {
 		.json({ status: 200, message: "This is the server response" });
 });
 
+// this is our catch all endpoint.
+app.get("*", (req, res) => {
+	res.status(404).json({
+		status: 404,
+		message: "This is obviously not what you are looking for.",
+	});
+});
+
 app.listen(port, () => {
 	console.info(`Listening on port ${port}`);
 });
